4ec068257f5ad97f0264e43e60745783baef9be0
Bruno Peixoto <brunopeixotorodrigues546@gmail.com>
2023-12-14 13:37:42 -0400

    curso-js-dio

--- Bloco-Exercícios/modulo1/aula2.js
+++ Bloco-Exercícios/modulo1/aula2.js
@@ -1,7 +1,7 @@
 /* Incremento do Cálculo de Valor de Uma Viagem
 *
-*
-*
+* O programa vai calcular o valor gasto de Etanol ou Gasolina de acordo com a preferência do usuário.
+* Programa desenvolvido para treinar condições e a sintax do JavaScript
 *
 *
 * @author: Bruno Rodrigues Peixoto
@@ -9,3 +9,22 @@
 * @javascript
 */
 
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmPOrLitros = 10;
+const distanciaEmKm = 100;
+
+const tipoCombustivel = 'Gasolina'
+
+
+if (tipoCombustivel === 'Etanol') {
+    const quantidadeDeCombustivelEtanol = precoEtanol/kmPOrLitros;
+    const valorDeEtanolParaViagem = quantidadeDeCombustivelEtanol*distanciaEmKm;
+    console.log(valorDeEtanolParaViagem);
+} else if (tipoCombustivel === "Gasolina") {
+    const quantidadeDeCombustivelGasolina = precoGasolina/kmPOrLitros;
+    const valorDeGasolinaParaViagem = quantidadeDeCombustivelGasolina*distanciaEmKm;
+    console.log(valorDeGasolinaParaViagem);
+} else {
+    console.log('Tipo de combustível inválido para o seu carro.')
+}
---
+++ Bloco-Exercícios/modulo1/aula3.js
@@ -0,0 +1,8 @@
+/* Média de Notas
+*
+*
+*
+* @author: Bruno Rodrigues Peixoto
+* @version 1.0
+* @JavaScript
+*/
